947cd0a315973ead20999efaa8b0367400262112
ohmyga <40896676+ohmyga233@users.noreply.github.com>
2020-06-21 12:52:24 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,3 +10,12 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#OStatus')
+
+if (process.env.NODE_ENV === 'production') {
+  console.clear();
+  console.log(
+    " %c ✨ OStatus %c https://github.com/ohmyga233/OStatus",
+    "color: #FFFFFF; background: #1E88E5; padding:6px;",
+    "color: #FFFFFF; background: #424242; padding:6px;"
+  )
+}
